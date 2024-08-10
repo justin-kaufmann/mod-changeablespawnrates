0781b45244e060b5855b81f1ab6632bc932d5a68
justin-kaufmann <86347766+justin-kaufmann@users.noreply.github.com>
2024-08-10 02:22:56 +0200

    Delete data/sql/db-world/base/custom_creatures.sql

--- data/sql/db-world/base/custom_creatures.sql
+++
@@ -1,7 +0,0 @@
--- Use world-database
-USE acore_world;
-
--- Create a new table with the same structure and data (IMPORTANT: THIS IS ONLY A BACKUP-TABLE!)
-CREATE TABLE custom_creature AS
-SELECT *
-FROM creature;
